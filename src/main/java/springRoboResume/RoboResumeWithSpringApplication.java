da7d0d1c8c235fb7f17fb3259297e0ef2a2d410b
Daniel Bonaparte <abonaparte3@yahoo.com>
2017-06-22 18:24:01 -0400

    first commit

---
+++ src/main/java/springRoboResume/RoboResumeWithSpringApplication.java
@@ -0,0 +1,12 @@
+package springRoboResume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoboResumeWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoboResumeWithSpringApplication.class, args);
+	}
+}
